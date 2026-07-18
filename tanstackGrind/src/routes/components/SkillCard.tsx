3a8828a720478a396811544ce518c71c24a68b4b
Lawrence <lawuche29@gmail.com>
2026-07-18 22:12:09 +0100

    info:so far basic react

--- tanstackGrind/src/routes/components/SkillCard.tsx
+++ tanstackGrind/src/routes/components/SkillCard.tsx
@@ -10,11 +10,17 @@ const index = ({ name }: SKillCardProps) => {
 	const likes = liked ? 1 : 0;
 
 	return (
-		<div>
-			Skills <h2>{name}</h2>
-			<p>{likes === 1 ? "likes" : "like"}</p>
-			<button type="button" onClick={() => setLiked(!liked)}>
+		<div className="w-[300px] h-max bg-green-800 text-white rounded-md p-2">
+			<p className="mb-3 uppercase flex flex-row items-center ">
+				Skills <h2 className="ml-2 font-bold">{name}</h2>
+			</p>
+			<button
+				type="button"
+				onClick={() => setLiked(!liked)}
+				className="flex flex-row items-center"
+			>
 				<Heart fill={liked ? "currentColor" : "none"} />
+				<p className="ml-2">{likes === 1 ? "likes" : "like"}</p>
 			</button>
 		</div>
 	);
