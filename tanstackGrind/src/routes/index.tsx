d6b49fbecab1d4ad90544f2a93dacd585ddff8e3
Lawrence <lawuche29@gmail.com>
2026-07-19 00:56:49 +0100

    info:this is very useful even has posthog as an addon

--- tanstackGrind/src/routes/index.tsx
+++ tanstackGrind/src/routes/index.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SkillCard from "./components/SkillCard";
 
-export const Route = createFileRoute("/")({ component: Home });
+export const Route = createFileRoute("/")({
+	component: Home,
+	loader: async () => {
+		const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+		const data = await res.json();
+		return data;
+	},
+	errorComponent: ({ error }) => {
+		return <p>Oops {error.message}</p>;
+	},
+	pendingMs: 500,
+});
 
 function Home() {
+	const data = Route.useLoaderData();
+
+	console.log(data, "HELLL");
 	return (
 		<div className="p-8 bg-neutral-400 text-white">
 			<SkillCard name={"OpenClaw"} />
